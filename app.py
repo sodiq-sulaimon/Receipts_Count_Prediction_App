60cb48de684b85f3d81ec4787b498349f809ed93
Sodiq Sulaimon <111898006+sodiq-sulaimon@users.noreply.github.com>
2023-11-22 23:17:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template
 import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
